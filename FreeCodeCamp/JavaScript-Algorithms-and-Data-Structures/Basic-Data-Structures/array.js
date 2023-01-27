6e03066e49b2d1754186649090a4ae21239023c6
iabwaheed <ab.waheedkashmiri@gmail.com>
2023-01-27 15:29:22 +0500

    Basic Array Methods

---
+++ FreeCodeCamp/JavaScript-Algorithms-and-Data-Structures/Basic-Data-Structures/array.js
@@ -0,0 +1,48 @@
+// ! Destructuring of Console
+const { log } = console
+
+// ? Task 1
+
+//!todo We have defined a variable called yourArray. Complete the statement by assigning an array of at least 5 elements in length to the yourArray variable. Your array should contain at least one string, one number, and one boolean.
+/* let yourArray
+yourArray = [
+  "Hello",
+  4,
+  [1, 2, 3, true],
+  { name: "Abdul Waheed", username: "abdulwaheed-apk" },
+  23,
+  false,
+]
+
+// log(yourArray)
+*/
+
+// ? Task 2
+
+/* let myArray = ["a", "b", "c", "d"]
+// Only change code below this line
+myArray[1] = "I am Changed"
+// Only change code above this line
+log(myArray) */
+
+// ? Task 3 Add Items to an Array with push() and unshift()
+
+/* function mixedNumbers(arr) {
+  // Only change code below this line
+  arr.unshift("I", 2, "three")
+  arr.push(7, "VIII", 9, { name: "jony", age: 20 })
+  // Only change code above this line
+  return arr
+}
+
+log(mixedNumbers(["IV", 5, "six"])) */
+
+// ? Task 4 Remove Items from an Array with pop() and shift()
+
+/* function popShift(arr) {
+  let popped = arr.pop() // Change this line
+  let shifted = arr.shift() // Change this line
+  return [shifted, popped]
+}
+
+log(popShift(["challenge", "is", "not", "complete"])) */
